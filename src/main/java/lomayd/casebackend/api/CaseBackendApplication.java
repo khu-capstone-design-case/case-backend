5c5a4576ceb25dea8da4a5744879bdb795bfe503
lomayd <jinho10167@gmail.com>
2023-10-24 19:47:25 +0900

    feat: Project Initialize

---
+++ src/main/java/lomayd/casebackend/api/CaseBackendApplication.java
@@ -0,0 +1,13 @@
+package lomayd.casebackend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/casebackend/CaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.casebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
